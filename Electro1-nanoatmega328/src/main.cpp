9b28c6324037f434269d8b4153dff479ba3fc008
agmkv70 <agmkv70@gmail.com>
2019-01-01 16:04:35 +0200

    analog pins to input

--- Electro1-nanoatmega328/src/main.cpp
+++ Electro1-nanoatmega328/src/main.cpp
@@ -15,30 +15,42 @@ volatile int channel;
 volatile unsigned long startMicros,endMicros;
 
 void setup()
-{ 
-  Serial.begin(115200);
+{ Serial.begin(115200);
   Serial.println("Start");
+  
+  pinMode(PIN_A0, INPUT);
+  pinMode(PIN_A1, INPUT);
+  pinMode(PIN_A2, INPUT);
+  pinMode(PIN_A3, INPUT);
+  pinMode(PIN_A4, INPUT);
+  pinMode(PIN_A5, INPUT);
+  pinMode(PIN_A6, INPUT);
+  pinMode(PIN_A7, INPUT);
+
   return;
-   //TIMSK0 = 0x00; // отключаем таймер (из-за прерываний)
-   //DIDR0 = 0x3F; // отключаем цифровые входы
 
-   //опорное AVCC (5V) для ATmega328
-   //установим 01
-   ADMUX =0; //&= ~((1<<REFS1) | (1<<REFS0));//очистили оба бита
-   ADMUX |= ((0<<REFS1) | (1<<REFS0)); //01 - тут просто берется напряжение питания
+  /////////////Настройка АЦП://///////////
+  //TIMSK0 = 0x00; // отключаем таймер (из-за прерываний)
+  //DIDR0 = 0x3F; // отключаем цифровые входы
 
-   //ADCSRA = 0xAC; // 1010 1100 включаем АЦП ADEN, разрешаем прерывания ADFR, делитель = 128 111
-   ADCSRA = (1<<ADEN)|(0<<ADSC)|(0<<ADATE)|(0<<ADIF)
-            |(1<<ADIE)
-            |(1<<ADPS2)|(1<<ADPS1)|(0<<ADPS0);
-    // set prescale to  64 110 //faster, medium (lower is more than 250kHz - so is not precise)
-    // set prescale to 128 111 //slow, fine
-    // sbi(ADCSRA,ADPS2) ;
-    // sbi(ADCSRA,ADPS1) ;
-    // cbi(ADCSRA,ADPS0) ;
-   //ADCSRB = 0x40; // Включаем каналы MUX АЦП, режим постоянной выборки
-   
+  //опорное AVCC (5V) для ATmega328
+  //установим 01
+  //ADMUX =0; //&= ~((1<<REFS1) | (1<<REFS0));//очистили оба бита
+  //ADMUX |= ((0<<REFS1) | (1<<REFS0)); //01 - тут просто берется напряжение питания
   channel=0; 
+  ADMUX=(0<<REFS1) | (1<<REFS0) | (channel); //01=
+
+  //ADCSRA = 0xAC; // 1010 1100 включаем АЦП ADEN, разрешаем прерывания ADFR, делитель = 128 111
+  ADCSRA = (1<<ADEN)|(0<<ADSC)|(0<<ADATE)|(0<<ADIF) //ADATE =  Auto Triggering; The ADC will start a conversion on a positive edge of the selected trigger signal. The trigger source is selected by setting the ADC Trigger Select bits, ADTS in SFIOR.
+          |(1<<ADIE)
+          |(1<<ADPS2)|(1<<ADPS1)|(0<<ADPS0);
+  // set prescale to  64 110 //faster, medium (lower is more than 250kHz - so is not precise)
+  // set prescale to 128 111 //slow, fine
+  // sbi(ADCSRA,ADPS2) ;
+  // sbi(ADCSRA,ADPS1) ;
+  // cbi(ADCSRA,ADPS0) ;
+  //ADCSRB = 0x40; // Включаем каналы MUX АЦП, режим постоянной выборки
+  
   sbi(ADCSRA, ADSC); // Запускаем преобразование установкой бита 6 (=ADSC) в ADCSRA
   sei(); // устанавливаем глобальный флаг прерываний
 }
@@ -80,13 +92,14 @@ void loop(){
 ISR(ADC_vect){
    if(ari>=512*NChannels) return; //just skip
   if(ari==0) startMicros=micros();
+
   channel = (++channel)%8;
-  ADMUX=(1<<REFS0)|(channel);
-  delayMicroseconds(10);
+  ADMUX=(0<<REFS1) | (1<<REFS0) | (channel);
+
+  delayMicroseconds(10); 
   sbi(ADCSRA, ADSC); //start next convertion
 
-  int analogValue = ADCL; // сохраняем младший байт АЦП
-  analogValue += ADCH << 8; // сохраняем старший байт АЦП
+  int analogValue = (ADCH << 8) | ADCL; // read both bytes (in ADCH only 2 lower bits are used)
   ar[ari] = analogValue;
 
   ari++;
